330fad5f08aa3a98c1fc10343a8a90893166d237
Jason Campbell <jasoncampbell@Jasons-MBP-2.lan>
2024-03-18 20:15:15 -0400

    class activity

--- 01-Lesson-Plans/02-Python-Programming-1/1/Activities/04-Stu_Variables/Unsolved/hello_variable_world.py
+++ 01-Lesson-Plans/02-Python-Programming-1/1/Activities/04-Stu_Variables/Unsolved/hello_variable_world.py
@@ -1,21 +1,22 @@
 # Create a variable called 'full_name' that holds a string
-
+full_name = "Jason Campbell"
 # Create a variable called 'country_to_visit' that holds a string
-
+country_to_visit = "japan"
 # Create a variable called 'days' that holds an integer of the number of days
 # for the vacation
-
+day = "14"
 # Create a variable called 'exchange_rate' that holds a float of the currency
 # exchange rate for the country to visit
-
+exchange_rate = 20.5
 # Create a variable called 'satisfied' that holds a Boolean
-
+satisfied = False
 # Print out "Hello <full_name>"
-
+print(full_name)
 # Print out a sentence with the country the user entered
-
+print(country_to_visit)
 # Print out a sentence with the number of vacation days
-
+print(day)
 # Print out a sentence with the currency exchange rate
-
+print(exchange_rate)
 # Print out a sentence with the value of satisfied
+print("not satisfied ", satisfied)
