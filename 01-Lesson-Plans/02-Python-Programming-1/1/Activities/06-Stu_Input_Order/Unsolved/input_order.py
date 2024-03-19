af80f81a14a882ba19165a817c2c90823b279793
Jason Campbell <jasoncampbell@Jasons-MBP-2.lan>
2024-03-19 18:35:57 -0400

    activity and tuesday class

--- 01-Lesson-Plans/02-Python-Programming-1/1/Activities/06-Stu_Input_Order/Unsolved/input_order.py
+++ 01-Lesson-Plans/02-Python-Programming-1/1/Activities/06-Stu_Input_Order/Unsolved/input_order.py
@@ -1,18 +1,20 @@
 # Take input of an item the user wants to purchase
-
+item_to_purchase = input("what are you buying? ")
 
 # Ask how much the item costs and cast it as a number.
 # What type of number should it be cast as?
-
+cost_of_item = float(input("how much does this item cost? "))
 
 # Ask what quantity of the item should be purchased and cast it as a number.
 # What type of number should it be cast as?
-
+quantity_of_item = int(input("quantity of item? "))
 
 # Print the item cost along with its data type
-
+print("float ", cost_of_item)
 
 # Print the item quantity along with its data type
-
+print("int", quantity_of_item)
 
 # Print results
+total_cost = cost_of_item * quantity_of_item
+print(f"the cost of the {item_to_purchase} is {total_cost}")
