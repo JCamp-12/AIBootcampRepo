9a3aea6dd90aaf6fb0b2b206667be52670875762
Jason Campbell <jasoncampbell@Jasons-MBP-2.lan>
2024-03-25 18:43:54 -0400

    classwork

--- 01-Lesson-Plans/02-Python-Programming-1/3/Activities/05-Stu_Dictionaries/Unsolved/hobby_dictionary.py
+++ 01-Lesson-Plans/02-Python-Programming-1/3/Activities/05-Stu_Dictionaries/Unsolved/hobby_dictionary.py
@@ -1,13 +1,27 @@
 # Dictionary full of info
 
-
+full_info = {
+    "name": "test",
+    "age": "21",
+    "hobbies": ["eat", "sleep", "walk dog"],
+    "wake-up": {
+        "Monday": "10:00",
+        "TuesDay": "9:00",
+        "Friday": "11:00"
+    }
+}
 # Print out results stored in the dictionary
-
+print(full_info)
 
 # Use a loop to print out the key-value pairs in the dictionary
-
-
+print('these are items: ', full_info.items())
+for items in full_info.items():
+    print('items in a loop ', items)
 # Use a loop to print out the keys of the wake-up dictionary
-
-
+print('these are keys: ', full_info.keys())
+for items in full_info.keys():
+    print('keys in a loop ', items)
 # Use a loop to print out the values of the wake-up dictionary
+print('these are values: ', full_info.values())
+for items in full_info.values():
+    print('values in a loop ', items)
